6cbb425acd8dc15c79502a55ae53bf80d95bf109
avatar-lavventura <alper.alimoglu@gmail.com>
2018-01-18 11:28:16 +0300

    update

--- server.sh
+++ server.sh
@@ -2,4 +2,6 @@
 DATADIR="/Users/alper";
 nohup geth --datadir $DATADIR/POA/private --rpcaddr 127.0.0.1 --rpc --rpcport 8545 --rpccorsdomain="*" --networkid 23422 --rpcapi eth,net,web3,personal &
 
+sleep 4
+
 echo "loadScript(\"$DATADIR"/POA/pass.js"\")"    | sudo geth --datadir "~/POA/private" attach ipc:$DATADIR/POA/private/geth.ipc console
