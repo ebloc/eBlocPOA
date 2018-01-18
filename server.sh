73cf8744f8bfe75256141ab2fef85a8698870766
avatar-lavventura <alper.alimoglu@gmail.com>
2018-01-18 11:24:34 +0300

    update

--- server.sh
+++ server.sh
@@ -1,5 +1,5 @@
-e#!/bin/bash
-nohup geth --datadir /home/alper/POA/private --rpcaddr 127.0.0.1 --rpc --rpcport 8546 --rpccorsdomain="*" --networkid 23422 --port 3016 --rpcapi eth,net,web3,personal &
+#!/bin/bash
+DATADIR="/Users/alper";
+nohup geth --datadir $DATADIR/POA/private --rpcaddr 127.0.0.1 --rpc --rpcport 8545 --rpccorsdomain="*" --networkid 23422 --rpcapi eth,net,web3,personal &
 
-echo 'loadScript("/home/alper/POA/pass.js")' | sudo geth --datadir "~/POA/private" attach ipc:/home/alper/POA/private/geth.ipc console
-echo 'miner.start()'                         | sudo geth --datadir "~/POA/private" attach ipc:/home/alper/POA/private/geth.ipc console
+echo "loadScript(\"$DATADIR"/POA/pass.js"\")"    | sudo geth --datadir "~/POA/private" attach ipc:$DATADIR/POA/private/geth.ipc console
