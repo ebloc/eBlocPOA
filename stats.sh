05e9a528c36fa7d382de42807473c47d923fc54c
avatar-lavventura <alper.alimoglu@gmail.com>
2018-02-04 17:37:08 +0300

    Create stats.sh

---
+++ stats.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#To see your node on http://ebloc.cmpe.boun.edu.tr:3015
+sudo pm2 delete app
+sudo INSTANCE_NAME=myNameIs RPC_HOST=localhost WS_SERVER=http://79.123.177.145:3015 WS_SECRET=63r98c3uz0cyg68v RPC_PORT=8545 LISTENING_PORT=30303 pm2 start $HOME/eth-net-intelligence-api/app.js
