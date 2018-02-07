f058e5d82e17ec497233797a6539036230899171
avatar-lavventura <alper.alimoglu@gmail.com>
2018-02-07 13:16:03 +0300

    Update server.sh

--- server.sh
+++ server.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-DATADIR="$HOME";
 sudo killall geth
-nohup geth --datadir $DATADIR/ebloc_POA/private --port 3000 --rpcaddr 127.0.0.1 --rpc --rpcport 8545 --rpccorsdomain="*" --networkid 23422 --rpcapi eth,net,web3,personal &
+DATADIR="$HOME/ebloc_POA";
+nohup geth --datadir $DATADIR/private --port 3000 --rpcaddr 127.0.0.1 --rpc --rpcport 8545 --rpccorsdomain="*" --networkid 23422 --rpcapi eth,net,web3,personal &
 
 sleep 6
 
-echo "loadScript(\"$DATADIR"/ebloc_POA/pass.js"\")"    | sudo geth --datadir "$DATADIR/ebloc_POA/private" attach ipc:$DATADIR/ebloc_POA/private/geth.ipc console
-echo "net"                                             | sudo geth --datadir "$DATADIR/ebloc_POA/private" attach ipc:$DATADIR/ebloc_POA/private/geth.ipc console
+echo "loadScript(\"$DATADIR"/pass.js"\")" | sudo geth --datadir "$DATADIR/private" attach ipc:$DATADIR/private/geth.ipc console
+echo "net"                                | sudo geth --datadir "$DATADIR/private" attach ipc:$DATADIR/private/geth.ipc console
