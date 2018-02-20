d28e35c49bd41e2371c2956e5faadb3dd0d4034c
eBloc <ebloc@protonmail.com>
2018-02-20 14:54:55 +0300

    Update stats.sh

--- stats.sh
+++ stats.sh
@@ -4,4 +4,4 @@ DATADIR="$HOME"
 NAME="myNameIs"
 
 sudo pm2 delete app
-sudo INSTANCE_NAME=$NAME RPC_HOST=localhost WS_SERVER=http://79.123.177.145:3015 WS_SECRET=63r98c3uz0cyg68v RPC_PORT=8545 LISTENING_PORT=30303 pm2 start $DATADIR/eth-net-intelligence-api/app.js
+sudo INSTANCE_NAME=$NAME RPC_HOST=localhost WS_SERVER=http://79.123.177.145:3015 WS_SECRET=63r98c3uz0cyg68v RPC_PORT=8545 LISTENING_PORT=3000 pm2 start $DATADIR/eth-net-intelligence-api/app.js
