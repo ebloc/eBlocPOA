d5a69d33724bc6820fcb4e281b1e183d35f7445e
eBloc <ebloc@protonmail.com>
2018-03-21 11:12:18 +0300

    Update server.sh

--- server.sh
+++ server.sh
@@ -9,8 +9,8 @@ sudo killall geth
 
 nohup geth --datadir $DATADIR/private --port $PORT --rpcaddr 127.0.0.1 --rpc --rpcport 8545 --rpccorsdomain="*" --networkid 23422 --rpcapi eth,net,web3,personal --gasprice "18000000000" > gethServer.out &
 
-echo "Please wait few seconds for geth-server to be activated."
-sleep 6
+echo "Please wait 10 seconds for geth-server to be activated. You can decrease the sleep time on server.sh."
+sleep 10
 
 echo "loadScript(\"$DATADIR"/peers.js"\")" | sudo geth --datadir "$DATADIR/private" attach ipc:$DATADIR/private/geth.ipc console
 echo "loadScript(\"$DATADIR"/pass.js"\")" | sudo geth --datadir "$DATADIR/private" attach ipc:$DATADIR/private/geth.ipc console
