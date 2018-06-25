95a34d46047e7a8bb6de6177678080b397cbfda8
eBloc <ebloc@protonmail.com>
2018-06-25 10:47:33 +0300

    Update server.sh

--- server.sh
+++ server.sh
@@ -14,7 +14,7 @@ if [ -n "$pid" ]; then
   sudo kill -9 $pid # sudo killall geth
 fi
 
-nohup geth --fast --cache=1024 --datadir $DATADIR/private --port $PORT --rpcaddr 127.0.0.1 --rpc --rpcport 8545 --rpccorsdomain="*" --networkid 23422 --rpcapi eth,net,web3,personal --gasprice "18000000000" > gethServer.out &
+nohup geth --shh --fast --cache=1024 --datadir $DATADIR/private --port $PORT --rpcaddr 127.0.0.1 --rpc --rpcport 8545 --rpccorsdomain="*" --networkid 23422 --rpcapi admin,eth,net,web3,debug,personal,shh --gasprice "18000000000" > gethServer.out &
 
 echo "Please wait 10 seconds for geth-server to be activated. You can decrease the sleep time on server.sh."
 sleep 10
