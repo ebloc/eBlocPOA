ef75d703e6a112ef650417465adc73464cd342fd
eBloc <ebloc@protonmail.com>
2018-07-25 12:56:31 +0300

    Update initialize.sh

--- initialize.sh
+++ initialize.sh
@@ -9,4 +9,23 @@ sed -i.bak "s/^\(DATADIR=\).*/\1\"$var\"/" server.sh && rm server.sh.bak
 sed -i.bak "s/^\(DATADIR=\).*/\1\"$var\"/" client.sh && rm client.sh.bak
 sed -i.bak "s/^\(DATADIR=\).*/\1\"$var\"/" stats.sh  && rm stats.sh.bak
 
+echo "Please enter name for eBloc Network Status:"
+read var
+
+if [[ $var == *\$* ]]; then
+    echo 'Please enter string without a dollar sign'
+fi
+
+if [[ $var == *\"* ]]; then
+    echo 'Please enter string without a double quote sign'
+    exit
+fi
+
+if [[ $var == *\'* ]]; then
+    echo 'Please enter string without a single quote sign'
+    exit
+fi
+
+sed -i.bak "s/^\(NAME=\).*/\1\"$var\"/" stats.sh && rm stats.sh.bak
+
 # npm install
