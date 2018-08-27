93a4074db4ae79d606e6df31c789678d387b364e
avatar-lavventura <alper.alimoglu@gmail.com>
2018-08-27 20:28:04 +0300

    server.sh small update

--- server.sh
+++ server.sh
@@ -5,7 +5,7 @@ git fetch && git checkout origin/master -- peers.js 2>/dev/null
 
 # Ensure running as root
 if [ "$(id -u)" != "0" ]; then
-  echo "sudo: Please run: sudo bash server.sh";
+  echo "sudo: Please run: sudo ./server.sh";
   exit
 fi
 
@@ -20,7 +20,7 @@ fi
 nohup geth --syncmode fast --cache=1024 --shh --datadir $DATADIR/private --port $PORT --rpcaddr 127.0.0.1 --rpc --rpcport 8545 --rpccorsdomain="*" --networkid 23422 --rpcapi admin,eth,net,web3,debug,personal,shh --gasprice "18000000000" > gethServer.out &
 
 SLEEP_DURATION=10;
-echo "Please wait $SLEEP_DURATION seconds for geth-server to be activated. You can decrease the sleep time on server.sh."
+echo "Please wait $SLEEP_DURATION seconds for geth-server to be activated before adding peers. You can decrease the sleep time on the server.sh script."
 sleep $SLEEP_DURATION
 
 echo "loadScript(\"$DATADIR"/peers.js"\")" | sudo geth --datadir "$DATADIR/private" attach ipc:$DATADIR/private/geth.ipc console
