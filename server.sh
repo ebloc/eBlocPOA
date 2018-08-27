01813a1f5c201710791281e9b0a4a9d368f2a2b1
avatar-lavventura <alper.alimoglu@gmail.com>
2018-08-27 15:26:53 +0300

    Update server.sh

--- server.sh
+++ server.sh
@@ -19,8 +19,9 @@ fi
 
 nohup geth --fast --cache=1024 --shh --datadir $DATADIR/private --port $PORT --rpcaddr 127.0.0.1 --rpc --rpcport 8545 --rpccorsdomain="*" --networkid 23422 --rpcapi admin,eth,net,web3,debug,personal,shh --gasprice "18000000000" > gethServer.out &
 
-echo "Please wait 10 seconds for geth-server to be activated. You can decrease the sleep time on server.sh."
-sleep 10
+SLEEP_DURATION=10;
+echo "Please wait $SLEEP_DURATION seconds for geth-server to be activated. You can decrease the sleep time on server.sh."
+sleep $SLEEP_DURATION
 
 echo "loadScript(\"$DATADIR"/peers.js"\")" | sudo geth --datadir "$DATADIR/private" attach ipc:$DATADIR/private/geth.ipc console
 echo "loadScript(\"$DATADIR"/pass.js"\")"  | sudo geth --datadir "$DATADIR/private" attach ipc:$DATADIR/private/geth.ipc console
